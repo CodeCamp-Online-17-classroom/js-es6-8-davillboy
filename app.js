2c5b540e580445a1556d2d2112a82eafae969624
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-24 16:41:29 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // write code here
+function removeRandom(arr) {
+  const copyArray = arr.slice();
+  const randomIndex = Math.floor(Math.random() * copyArray.length);
+  copyArray.splice(randomIndex, 1);
+  return copyArray;
+}
+
+const originalArray = [1, 2, 3, 4, 5];
+const modifiedArray = removeRandom(originalArray);
+
+console.log(originalArray); 
+console.log(modifiedArray); 
